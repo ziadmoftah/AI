5592a456974389008a66c994c3d7cde335de3d64
Mohamed <mohamed.zaki78963@gmail.com>
2020-12-11 19:42:59 +0200

    Added Project

---
+++ AI-Package/SearchAlgorithms/SearchAlgorithms.py
@@ -0,0 +1,75 @@
+class Node:
+    id = None  # Unique value for each node.
+    up = None  # Represents value of neighbors (up, down, left, right).
+    down = None
+    left = None
+    right = None
+    previousNode = None  # Represents value of neighbors.
+    edgeCost = None  # Represents the cost on the edge from any parent to this node.
+    gOfN = None  # Represents the total edge cost
+    hOfN = None  # Represents the heuristic value
+    heuristicFn = None  # Represents the value of heuristic function
+
+    def __init__(self, value):
+        self.value = value
+
+
+class SearchAlgorithms:
+    ''' * DON'T change Class, Function or Parameters Names and Order
+        * You can add ANY extra functions,
+          classes you need as long as the main
+          structure is left as is '''
+    path = []  # Represents the correct path from start node to the goal node.
+    fullPath = []  # Represents all visited nodes from the start node to the goal node.
+    totalCost = -1  # Represents the total cost in case using UCS, AStar (Euclidean or Manhattan)
+
+    def __init__(self, mazeStr, heristicValue=None):
+        ''' mazeStr contains the full board
+         The board is read row wise,
+        the nodes are numbered 0-based starting
+        the leftmost node'''
+        pass
+
+    def DLS(self):
+        # Fill the correct path in self.path
+        # self.fullPath should contain the order of visited nodes
+        return self.path, self.fullPath
+
+    def BDS(self):
+        # Fill the correct path in self.path
+        # self.fullPath should contain the order of visited nodes
+        return self.path, self.fullPath
+
+    def BFS(self):
+        # Fill the correct path in self.path
+        # self.fullPath should contain the order of visited nodes
+        return self.path, self.fullPath, self.totalCost
+
+
+
+def main():
+    searchAlgo = SearchAlgorithms('S,.,.,#,.,.,. .,#,.,.,.,#,. .,#,.,.,.,.,. .,.,#,#,.,.,. #,.,#,E,.,#,.')
+    path, fullPath = searchAlgo.DLS()
+    print('**DFS**\nPath is: ' + str(path) + '\nFull Path is: ' + str(fullPath) + '\n\n')
+
+                #######################################################################################
+
+    searchAlgo = SearchAlgorithms('S,.,.,#,.,.,. .,#,.,.,.,#,. .,#,.,.,.,.,. .,.,#,#,.,.,. #,.,#,E,.,#,.')
+    path, fullPath = searchAlgo.BDS()
+    print('**BFS**\nPath is: ' + str(path) + '\nFull Path is: ' + str(fullPath) + '\n\n')
+                #######################################################################################
+
+    searchAlgo = SearchAlgorithms('S,.,.,#,.,.,. .,#,.,.,.,#,. .,#,.,.,.,.,. .,.,#,#,.,.,. #,.,#,E,.,#,.', [0, 15, 2, 100, 60, 35, 30, 3
+                                                                                                             , 100, 2, 15, 60, 100, 30, 2
+                                                                                                             , 100, 2, 2, 2, 40, 30, 2, 2
+                                                                                                             , 100, 100, 3, 15, 30, 100, 2
+                                                                                                             , 100, 0, 2, 100, 30])
+    path, fullPath, TotalCost = searchAlgo.BFS()
+    print('** UCS **\nPath is: ' + str(path) + '\nFull Path is: ' + str(fullPath) + '\nTotal Cost: ' + str(
+        TotalCost) + '\n\n')
+               #######################################################################################
+
+
+
+
+main()
